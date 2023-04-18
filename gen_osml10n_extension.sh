4b5435d96464b533889752d91f3849759cc64892
Paul van Eyk <paul.vaneyk@spiker.co.nz>
2023-04-18 14:07:10 +1200

    Update gen_osml10n_extension.sh
    https://www.nominatim.org/data/country_grid.sql.gz needs browser headers to work again

--- gen_osml10n_extension.sh
+++ gen_osml10n_extension.sh
@@ -20,7 +20,7 @@ done
 if ! [ -f "country_osm_grid.sql" ]; then
   rm -f country_osm_grid.sql
   echo -n "Trying to download country_grid.sql.gz from nominatim.org... "
-  curl -sL https://www.nominatim.org/data/country_grid.sql.gz |gzip -d >country_osm_grid.sql
+  curl -sL -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36" https://www.nominatim.org/data/country_grid.sql.gz |gzip -d >country_osm_grid.sql  
 
   if ! [ -s country_osm_grid.sql ]; then
     rm -f country_osm_grid.sql
